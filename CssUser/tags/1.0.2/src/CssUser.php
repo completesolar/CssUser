61dc90a6e685a1149b093404ee4e7b270f8261c3
alanma <alanma@d3ea97c9-f87a-4f39-b3ea-c0debacafb09>
2014-04-29 15:59:27 +0000

    Tagging Vs. 1.0.2

---
+++ CssUser/tags/1.0.2/src/CssUser.php
@@ -0,0 +1,39 @@
+<?php
+namespace CompleteSolar\User;
+
+class CssUser {
+    private $id;
+    private $email;
+    private $firstName;
+    private $lastName;
+    private $image;
+
+    public function __construct($id, $email, $firstName, $lastName, $image){
+        $this->id = $id;
+        $this->email = $email;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->image = $image;
+    }
+
+    public function getID(){
+        return $this->id;
+    }
+
+    public function getEmail(){
+        return $this->email;
+    }
+
+    public function getFirstName(){
+        return $this->firstName;
+    }
+
+    public function getLastName(){
+        return $this->lastName;
+    }
+    
+    public function getImage(){
+        return $this->image;
+    }
+}
+?>
